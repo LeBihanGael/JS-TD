cab57210c69ed93fe6559ce21d8a18e243945688
LeBihanGael <gael.lebihan1606@gmail.com>
2025-12-15 17:24:51 +0100

    ajout function exo 7

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 N = prompt("Entrer un nombre N : ");
 var facto = 0;
-for (var i = 1; i <= N; i++) {
-    facto += i;
+for (var i = 1; i <= N-1; i++) {
+    facto = Addition(facto, i);
 }
 alert("La facto est égal à : " + facto);
+
+function Addition(param1, param2) {
+    return param1 + param2;
+}
+
+
+// ancienne version de l'exo6 facto = facto + i;
